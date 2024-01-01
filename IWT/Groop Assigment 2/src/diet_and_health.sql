6d5741ed1c1a0aaaa84c155f8020e9c82c36132f
JAHAN RAMESH <121393192+JahanRazh@users.noreply.github.com>
2024-01-01 23:59:54 +0530

    Add files via upload

--- IWT/Groop Assigment 2/src/diet_and_health.sql
+++ IWT/Groop Assigment 2/src/diet_and_health.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 06, 2023 at 09:16 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Jan 01, 2024 at 05:27 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,20 +37,20 @@ CREATE TABLE `appointment` (
   `doctor_name` varchar(255) NOT NULL,
   `city` varchar(100) NOT NULL,
   `date` date NOT NULL,
-  `Time` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Time` varchar(100) NOT NULL,
+  `userID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`aid`, `name`, `email`, `gender`, `age`, `doctor_name`, `city`, `date`, `Time`) VALUES
-(21, 'chamoth dilshan', 'kcdilshan123@gmail.com', 'Male', 23, 'Dr.priyantha', 'udubaddawa', '2023-06-14', '12.30-2.30 PM'),
-(22, 'nipun lakshan', 'nipun123@gmail.com', 'Male', 32, 'Dr.gunawardane', 'kuliyapitiya', '2023-06-30', '8.30-10.30 AM'),
-(23, 'naveen fernando', 'naveen33@gmail.com', 'Male', 20, 'Dr.priyantha', 'colombo', '2023-06-22', '8.30-10.30 AM'),
-(24, 'ranudi imalsha', 'ranudi@gmail.com', 'Female', 22, 'Dr.thilakawardene', 'wattala', '2023-07-08', '2.30-4.30 PM'),
-(25, 'tharindi perera', 'tharindi069@gmail.com', 'Female', 30, 'Dr.gunawardane', 'colombo', '2023-06-29', '12.30-2.30 PM'),
-(26, 'fcewe', 'dewq@h.com', 'Male', 2, 've', 'fe', '2023-06-13', '10.30-12.30 PM');
+INSERT INTO `appointment` (`aid`, `name`, `email`, `gender`, `age`, `doctor_name`, `city`, `date`, `Time`, `userID`) VALUES
+(21, 'chamoth dilshan', 'kcdilshan123@gmail.com', 'Male', 23, 'Dr.priyantha', 'udubaddawa', '2023-06-14', '12.30-2.30 PM', 0),
+(23, 'naveen fernando', 'naveen33@gmail.com', 'Male', 20, 'Dr.priyantha', 'colombo', '2023-06-22', '8.30-10.30 AM', 0),
+(24, 'ranudi imalsha', 'ranudi@gmail.com', 'Female', 22, 'Dr.thilakawardene', 'wattala', '2023-07-08', '2.30-4.30 PM', 0),
+(25, 'tharindi perera', 'tharindi069@gmail.com', 'Female', 30, 'Dr.gunawardane', 'colombo', '2023-06-29', '12.30-2.30 PM', 0),
+(26, 'fcewe', 'dewq@h.com', 'Male', 2, 've', 'fe', '2023-06-13', '10.30-12.30 PM', 0);
 
 -- --------------------------------------------------------
 
@@ -68,15 +69,15 @@ CREATE TABLE `diatplanappointment` (
   `City` char(50) NOT NULL,
   `Date` date NOT NULL,
   `Time` char(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `diatplanappointment`
 --
 
 INSERT INTO `diatplanappointment` (`Appid`, `Fullname`, `Email`, `Gender`, `Age`, `Pnumber`, `Nutrition_name`, `City`, `Date`, `Time`) VALUES
-(82, 'kanishka', 'behan@gmail.com', 'Male', 23, 2147483647, 'SDAD', 'matale', '2023-06-14', '8.30-10.30 AM'),
-(84, 'Mr.Jahan Ramesh', 'jahanrazh@gmail.com', 'Male', 53, 767221436, 'Mr.kumara siriwardhana', 'colombo', '2023-10-25', '10.30-12.30 PM');
+(1, 'Mr.kumara Disanayaka', 'www.kumara3165@gmail.com', 'Male', 25, 767445689, 'Mr.smarakoon', 'Kurunegala', '2023-07-31', '8.30-10.30 AM'),
+(81, 'Mrs.wiwiyam Randunupura', 'wiwiyanrandunupura@gmail.com', 'Female', 58, 377221436, 'Mr. wasantha', 'kurunegala', '2024-01-18', '8.30-10.30 AM');
 
 -- --------------------------------------------------------
 
@@ -97,16 +98,17 @@ CREATE TABLE `payment_details` (
   `month` char(20) NOT NULL,
   `year` int(50) NOT NULL,
   `cvv` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `payment_details`
 --
 
 INSERT INTO `payment_details` (`Payid`, `fullname`, `email`, `Address`, `city`, `state`, `zipcode`, `ownername`, `cardno`, `month`, `year`, `cvv`) VALUES
-(14, 'behan', 'behan@gmail.com', 'galewela', 'matale', 'srilanka', 123456, 'behan', 2147483647, 'march', 2022, 1234),
-(15, 'kanishka', 'kanishka2@gmail.com', 'Galewela', 'matale', 'Srilanka', 123456, 'Kanishka GKP', 2147483647, 'May', 2023, 1234),
-(16, 'swwsqw', 'kanishka2@gmail.com', 'ewdew', 'ewdewd', 'ewd', 123456, 'd', 2147483647, 'WEDD', 2022, 1234);
+(1, 'ganesh pathirana', 'ganeshpathirana@gmail.com', 'kawisigamuwa udagama', 'kurunegala', 'sri lanaka', 524247, 'g pathirana', 4563287, 'januvary', 2025, 789),
+(2, 'sulakshana karunathilaka ', 'sulakshanakarunathilaka@gmail.com ', 'udulupola,morathiaha', 'kandy', 'sri lanaka', 224852, 's karunathilaka', 7892465, 'february', 2024, 258),
+(3, 'kamal namal', 'kamalnamal@gmail.com', 'dodangaslanda,reedigama', 'jaffna', 'sri lanaka', 756148, 'k namal', 1247532, 'march', 2026, 212),
+(4, 'kumara siriwardhana', 'kumarasiriwardhana@gmail.com', 'monaragala,monargala', 'colombo', 'sri lanaka', 963247, 'k siriwardhana', 1242478, 'december', 2030, 122);
 
 -- --------------------------------------------------------
 
@@ -124,7 +126,7 @@ CREATE TABLE `register` (
   `country` varchar(50) NOT NULL,
   `address` varchar(100) NOT NULL,
   `nic` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `register`
@@ -132,7 +134,7 @@ CREATE TABLE `register` (
 
 INSERT INTO `register` (`RID`, `Fname`, `Lname`, `Email`, `Password`, `phoneNumber`, `country`, `address`, `nic`) VALUES
 (40, 'kanishka', 'wijekoon', 'kanishka2@gmail.com', '123', 714985025, 'srilanka', 'Galewela', '2000019001080'),
-(42, 'jahan', 'ramesh', 'jahanrazh@gmail.com', 'Razh@3165', 767221436, 'sri lanak', 'kawisigamuwa morathiha', '200013502372');
+(41, 'JAHAN', 'RAMESH', 'jahanrazh@gmail.com', 'Razh@3165', 767221436, 'sri lanaka', 'kawisigamuwa,Morathiha', '200013502372');
 
 -- --------------------------------------------------------
 
@@ -150,14 +152,20 @@ CREATE TABLE `voucher` (
   `Date` date NOT NULL,
   `Phone_Number` int(10) NOT NULL,
   `Payment_Type` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `voucher`
 --
 
 INSERT INTO `voucher` (`VID`, `Name`, `VNumber`, `Email`, `Plan`, `Reason`, `Date`, `Phone_Number`, `Payment_Type`) VALUES
-(17, '34at4t', 2, '3r@gg.com', 'rd2w', '2rd32', '2023-06-30', 714985025, 'Online Payment');
+(1, 'Avishka', 0, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-08', 776237073, 'Online Payment'),
+(3, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-09', 776237073, 'Pysical Payment'),
+(5, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-09', 776237073, 'Pysical Payment'),
+(6, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-09', 776237073, 'Pysical Payment'),
+(7, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-10', 776237073, 'Online Payment'),
+(8, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-10', 776237073, 'Online Payment'),
+(9, 'Avishka', 123, 'avishkaniraj2001@gmail.com', 'Health checkup', 'Mental', '2023-06-10', 776237073, 'Online Payment');
 
 --
 -- Indexes for dumped tables
@@ -201,31 +209,31 @@ ALTER TABLE `voucher`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `diatplanappointment`
 --
 ALTER TABLE `diatplanappointment`
-  MODIFY `Appid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `Appid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
 
 --
 -- AUTO_INCREMENT for table `payment_details`
 --
 ALTER TABLE `payment_details`
-  MODIFY `Payid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Payid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
-  MODIFY `RID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `RID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `voucher`
 --
 ALTER TABLE `voucher`
-  MODIFY `VID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `VID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
