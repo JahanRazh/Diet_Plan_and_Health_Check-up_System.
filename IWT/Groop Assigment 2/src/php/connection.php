0fe1e3b3c9012c1d568049325d165d032bbe940e
JAHAN RAMESH <121393192+JahanRazh@users.noreply.github.com>
2025-01-06 00:25:02 +0530

    Update connection.php

--- IWT/Groop Assigment 2/src/php/connection.php
+++ IWT/Groop Assigment 2/src/php/connection.php
@@ -1,18 +1,18 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "localhost"; // MAMP uses localhost
+$username = "root";        // Default username in MAMP is 'root'
+$password = "root";        // Default password in MAMP is 'root'
 $db_name = "diet_and_health";
+$port = 3308;              // MAMP's MySQL default port
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
+$conn = new mysqli($servername, $username, $password, $db_name, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
-else {
-      //echo "Connected successfully";
-    }  
+} else {
+    echo "Connected successfully";
+}
 ?>
